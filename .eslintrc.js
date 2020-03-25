cfb8c93dc9883642eb710c08a41c3d7933ca8fd0
victor-osf <victor.rocha@ifactory.com.br>
2020-03-25 15:35:48 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase" : "off",
+    "no-unused-vars" : ["error", { "argsIgnorePattenr" : "next" }],
+    "linebreak-style": 0
+  },
+};
